12426c15a26c987fb6af97d8528942e383c12794
Ben <57039667+ben-qnimble@users.noreply.github.com>
2020-10-23 07:08:42 -0400

    Halt when debugging enabled but no debugger connected. Prevents hard fault

--- quarto/debugprintf.c
+++ quarto/debugprintf.c
@@ -29,7 +29,35 @@ static void puint_debug(unsigned int num)
 #define putchar_debug putchar
 
 
-FLASHMEM void printf_debug_init(void){};
+#define CoreDebug_BASE (0xE000EDF0UL) /*!< Core Debug Base Address */
+#define CoreDebug ((CoreDebug_Type *) CoreDebug_BASE) /*!< Core Debug configuration struct */
+
+
+typedef struct
+{
+	volatile uint32_t DHCSR;                  /*!< Offset: 0x000 (R/W)  Debug Halting Control and Status Register */
+	volatile  uint32_t DCRSR;                  /*!< Offset: 0x004 ( /W)  Debug Core Register Selector Register */
+	volatile uint32_t DCRDR;                  /*!< Offset: 0x008 (R/W)  Debug Core Register Data Register */
+	volatile uint32_t DEMCR;                  /*!< Offset: 0x00C (R/W)  Debug Exception and Monitor Control Register */
+} CoreDebug_Type;
+
+#define CoreDebug_DHCSR_C_DEBUGEN_Pos       0U                                            /*!< CoreDebug DHCSR: C_DEBUGEN Position */
+#define CoreDebug_DHCSR_C_DEBUGEN_Msk      (1UL /*<< CoreDebug_DHCSR_C_DEBUGEN_Pos*/)     /*!< CoreDebug DHCSR: C_DEBUGEN Mask */
+
+
+
+FLASHMEM void printf_debug_init(void){
+	GPIO8_GDIR = 0x07;
+	GPIO8_DR_CLEAR = 0x7;
+	if ((CoreDebug->DHCSR &  CoreDebug_DHCSR_C_DEBUGEN_Msk) != CoreDebug_DHCSR_C_DEBUGEN_Msk) {
+	    //Debugging enabled, but not in debug mode, so halt and prevent crash
+		GPIO8_DR_SET = 0x7;
+	    while(1){}
+	}
+
+
+
+};
 
 
 #endif // PRINT_DEBUG_STUFF
