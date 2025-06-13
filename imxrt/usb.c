b9dc19f2a7738facb09d68e22c0e86adf4c20328
Ben <57039667+ben-qnimble@users.roreply.github.com>
2025-06-13 07:07:43 -0700

    Make USB interrupt safe without long time periods with interrupts disabled. Maybe fix USB data transfer stops

--- imxrt/usb.c
+++ imxrt/usb.c
@@ -1010,16 +1010,15 @@ static void schedule_transfer(endpoint_t *endpoint, uint32_t epmask, transfer_t 
 	if (endpoint->callback_function) {
 		transfer->status |= (1<<15);
 	}
-#ifndef ARDUINO_QUARTO
 	__disable_irq();
-#endif
-	//digitalWriteFast(1, HIGH);
 	// Executing A Transfer Descriptor, page 2468 (RT1060 manual, Rev 1, 12/2018)
 	transfer_t *last = endpoint->last_transfer;
 	if (last) {
 		last->next = (uint32_t)transfer;
-		if (USB1_ENDPTPRIME & epmask) goto end;
-		//digitalWriteFast(2, HIGH);
+		uint32_t prime_check = USB1_ENDPTPRIME & epmask;
+		__enable_irq();
+
+		if (prime_check) goto end_irq_enabled;
 		//ret |= 0x01;
 		uint32_t status, cyccnt=ARM_DWT_CYCCNT;
 		do {
@@ -1027,23 +1026,31 @@ static void schedule_transfer(endpoint_t *endpoint, uint32_t epmask, transfer_t 
 			status = USB1_ENDPTSTATUS;
 		} while (!(USB1_USBCMD & USB_USBCMD_ATDTW) && (ARM_DWT_CYCCNT - cyccnt < 2400));
 		//USB1_USBCMD &= ~USB_USBCMD_ATDTW;
-		if (status & epmask) goto end;
+		if (status & epmask) goto end_irq_enabled;
 		//ret |= 0x02;
+		__disable_irq();
 		endpoint->next = (uint32_t)transfer;
 		endpoint->status = 0;
 		USB1_ENDPTPRIME |= epmask;
 		goto end;
 	}
 	//digitalWriteFast(4, HIGH);
+	__disable_irq();
 	endpoint->next = (uint32_t)transfer;
 	endpoint->status = 0;
 	USB1_ENDPTPRIME |= epmask;
 	endpoint->first_transfer = transfer;
 end:
 	endpoint->last_transfer = transfer;
-#ifndef ARDUINO_QUARTO
 	__enable_irq();
-#endif
+	return;
+
+end_irq_enabled:
+__disable_irq();
+	endpoint->last_transfer = transfer;
+__enable_irq();
+	return;
+
 	//digitalWriteFast(4, LOW);
 	//digitalWriteFast(3, LOW);
 	//digitalWriteFast(2, LOW);
